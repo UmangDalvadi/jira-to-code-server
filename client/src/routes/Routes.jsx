8203f649c247195335bc0093fe5f66ab18937b34
sneha0099 <snehaprajapati678@gmail.com>
2025-06-24 22:09:35 +0530

    add changes in routes page

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import JiraAuthLoading from "../pages/RedirectLoadingPage";
-//import React from "react";
+
 
 export default function AppRoutes() {
   return (
